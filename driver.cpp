ab78cdc7c1e344b2088a6a4485edc492900e77b3
Arpit Joshi <joshiarpit2@gmail.com>
2019-05-10 07:53:24 +0530

    edited driver

--- driver.cpp
+++ driver.cpp
@@ -8,36 +8,14 @@ using namespace std;
 int main()
 {
 	int choice;
-	cout<<"1. Encrypt\n2. Decrypt\nEnter option :";
-	cin>>choice;
-	if(choice==1)
-	{
-		string answer;
-		cout<<"NOTE - MAKE SURE YOU HAVE ADDED THE PLAIN TEXT IN THE PLAINTEXT FILE(testing/input/plainText.txt).\nDo you want to continue?(yes/no) :";
-		cin>>answer;
-		if(answer=="y" or answer=="yes")
-		{
-			Encryption e;
-			e.encryptDriver();
-			cout<<"[+] Completed.";
-			
-		}
-		else cout<<"WRONG";
-	}
-	else if(choice==2)
-	{
-		string answer;
-		cout<<"Do you want to continue?(yes/no) :";
-		cin>>answer;
-		cout<<answer;
-		if(answer=="y" or answer=="yes")
-		{
-			Decryption d;
-			d.decrypt();
-			cout<<"[+] Completed";
-			exit(0);
-		}
-		else exit(0);
-
-	}
+	// cout<<"111. Encrypt\n2. Decrypt\nEnter option :";
+	cout<<"[+] Encrypting then decrypting..";
+	Encryption e;
+	e.encryptDriver();
+	cout<<"[+] Encryption Completed.";
+	Decryption d;
+	d.decrypt();
+	cout<<"[+] Decryption Completed";
+	exit(0);
+	
 }
